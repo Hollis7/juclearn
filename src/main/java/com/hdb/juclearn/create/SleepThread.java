e2f102661c39ab86b2701d401b5dc7b0f03565f9
hollis <herryhollis@163.com>
2024-03-04 16:08:16 +0800

    sleep

---
+++ src/main/java/com/hdb/juclearn/create/SleepThread.java
@@ -0,0 +1,23 @@
+package com.hdb.juclearn.create;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class SleepThread {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2L);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("睡眠完毕");
+            }
+        });
+        t1.start();
+        // 线程中断
+//        t1.interrupt();
+    }
+}
