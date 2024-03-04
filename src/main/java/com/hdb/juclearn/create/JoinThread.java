b1b7641eefaea022e37d5b70f58ce150136b6e32
hollis <herryhollis@163.com>
2024-03-04 21:35:33 +0800

    线程合并（同步）

--- src/main/java/com/hdb/juclearn/create/JoinThread.java
+++ src/main/java/com/hdb/juclearn/create/JoinThread.java
@@ -1,4 +1,27 @@
 package com.hdb.juclearn.create;
 
+import java.util.concurrent.TimeUnit;
+
 public class JoinThread {
+    static int value = 1;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new Thread(() -> {
+            // todo .. 业务
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            value = 10;
+            System.out.println("线程Runnable");
+        });
+        t1.start();// 异步
+        System.out.println(t1.isAlive());
+        t1.join(1000);  // 异步变成同步
+        System.out.println(t1.isAlive());
+        System.out.println("主线程:" + value);
+    }
+
+
 }
