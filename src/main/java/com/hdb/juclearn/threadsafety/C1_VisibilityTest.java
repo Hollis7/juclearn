780fe12308ef342ed7d7402f85cb31b96ceaa3ef
hollis <herryhollis@163.com>
2024-03-08 15:28:43 +0800

    可视化

---
+++ src/main/java/com/hdb/juclearn/threadsafety/C1_VisibilityTest.java
@@ -0,0 +1,18 @@
+package com.hdb.juclearn.threadsafety;
+
+public class C1_VisibilityTest {
+    static volatile Boolean always = true;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            while (always) {
+//                System.out.println("执行了....");
+//                synchronized (always) {
+//                }
+            }
+
+        }).start();
+        Thread.sleep(2000);
+        always = false;
+    }
+}
