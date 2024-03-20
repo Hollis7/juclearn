9cc350d81eb092fbf91a17e976273d62378e5868
hollis <herryhollis@163.com>
2024-03-20 19:53:23 +0800

    双重检查锁

---
+++ src/main/java/com/hdb/juclearn/threadsafety/Singleton.java
@@ -0,0 +1,20 @@
+package com.hdb.juclearn.threadsafety;
+
+public class Singleton {
+    public volatile static Singleton instance;
+
+    private Singleton() {
+
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+}
