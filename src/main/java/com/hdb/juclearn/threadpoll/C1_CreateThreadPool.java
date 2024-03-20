14188f20bbb6a2814ff6aea58563e5b83056b7a6
hollis <herryhollis@163.com>
2024-03-20 19:54:23 +0800

    线程池

--- src/main/java/com/hdb/juclearn/threadpoll/C1_CreateThreadPool.java
+++ src/main/java/com/hdb/juclearn/threadpoll/C1_CreateThreadPool.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class C1_CreateThreadPool {
