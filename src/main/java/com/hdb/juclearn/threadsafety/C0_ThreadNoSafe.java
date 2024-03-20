a0b997579c508bbf80345f5357154f8ca9aa48d2
hollis <herryhollis@163.com>
2024-03-20 19:54:43 +0800

    线程不安全

--- src/main/java/com/hdb/juclearn/threadsafety/C0_ThreadNoSafe.java
+++ src/main/java/com/hdb/juclearn/threadsafety/C0_ThreadNoSafe.java
@@ -3,10 +3,11 @@ package com.hdb.juclearn.threadsafety;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class C0_ThreadNoSafe {
     public static Integer stock = 100000;
+    public static AtomicInteger atomicStock = new AtomicInteger(100000);
 
     static class StockRunnable implements Runnable {
         @Override
@@ -15,6 +16,9 @@ public class C0_ThreadNoSafe {
                 // 购买.. stock=stock-1； 1000000-1
                 //stock--;
                 stock--;
+            }
+            if (atomicStock.get() > 0) {
+                atomicStock.decrementAndGet();
             }
         }
     }
@@ -37,5 +41,6 @@ public class C0_ThreadNoSafe {
             threadPool.awaitTermination(1000, TimeUnit.SECONDS);
         }
         System.out.println("剩余库存：" + stock);
+        System.out.println("剩余库存：" + atomicStock.get());
     }
 }
