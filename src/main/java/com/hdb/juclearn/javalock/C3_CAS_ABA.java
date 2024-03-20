c06c692fd41631b489b2d84e3ea5a243cc41b9c0
hollis <herryhollis@163.com>
2024-03-20 20:17:27 +0800

    ABA问题

---
+++ src/main/java/com/hdb/juclearn/javalock/C3_CAS_ABA.java
@@ -0,0 +1,37 @@
+package com.hdb.juclearn.javalock;
+
+import java.util.concurrent.atomic.AtomicStampedReference;
+
+public class C3_CAS_ABA {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 张三余额
+        AtomicStampedReference<Integer> balance = new AtomicStampedReference(1000, 0);
+
+        System.out.println("（A）张三余额:" + balance.getReference());
+
+        // 财务发3000工资
+        balance.compareAndSet(balance.getReference(), 4000, balance.getStamp(), balance.getStamp() + 1);
+        System.out.println("（B）财务发3000工资:" + balance.getReference());
+
+
+        // 老婆取3000工资
+        balance.compareAndSet(balance.getReference(), 1000, balance.getStamp(), balance.getStamp() + 1);
+
+        System.out.println("（A）老婆取3000工资:" + balance.getReference());
+
+
+        // 张三查工资
+        if (balance.getReference() > 3000) {
+
+            System.out.println("张三美滋滋" + balance.getReference());
+        } else {
+            if (balance.getStamp() == 1) {
+                System.out.println("张三找财务麻烦:");
+            } else {
+                System.out.println("张三找老婆麻烦:");
+            }
+
+        }
+    }
+}
